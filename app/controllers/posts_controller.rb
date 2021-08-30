59a647f987812b3814c2b43f8f8ebacb2e292dde
skandows <skandows@gmail.com>
2021-08-30 17:06:08 +0300

    Edit posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = Post.new(posts_params)
 
     if @post.save
       redirect_to @post
@@ -21,8 +21,28 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+def update
+  @post = Post.find(params[:id])
+
+  if @post.update(params[:post].permit(:tittle, :body))
+    redirect_to @post
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+
+  redirect_to posts_path
+end
   private
-    def post_params
+    def posts_params
       params.require(:post).permit(:title, :body)
     end
 end
