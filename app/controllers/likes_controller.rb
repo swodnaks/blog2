743668744bada5355c54354dbe446c13e9fd4664
skandows <skandows@gmail.com>
2021-09-17 15:23:00 +0300

    LikesLimit3

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,32 +3,31 @@ class LikesController < ApplicationController
 
   def create
     #тут GenerateLikesJob.perform_later
-  if already_liked?
-    flash[:notice] = "You can't like more than once"
-  else
-  if own_like
-    flash[:notice] = "You can't like your own post"
-  else
-    @post.likes.create(user_id: current_user.id)
+    if already_liked?
+      flash[:notice] = "You can't like more than once"
+    else
+      if own_like
+      flash[:notice] = "You can't like your own post"
+    else
+      @post.likes.create(user_id: current_user.id)
+    end
+      redirect_to post_path(@post)
+    end
   end
-    redirect_to post_path(@post)
-  end
-end
 
   private
 
   def already_liked?
-     Like.where(user_id: current_user.id, post_id:
-     params[:post_id]).exists?
+    Like.where(user_id: current_user.id, post_id:
+    params[:post_id]).exists?
   end
 
   def find_post
-     @post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
   end
 
   def own_like
-     Like.find_by(user_id: @post.user_id == current_user.id , post_id:
-     @post.user_id)
+    Like.find_by(user_id: @post.user_id == current_user.id, post_id:
+    @post.user_id)
   end
-
 end
